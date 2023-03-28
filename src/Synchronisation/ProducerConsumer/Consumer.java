9ba2931a3f321b71d4a6332659f07baeef87ddec
Stryker316 <ansil.mishra316@gmail.com>
2023-03-29 02:10:59 +0530

    Producer Cosumer with Mutex Locks

---
+++ src/Synchronisation/ProducerConsumer/Consumer.java
@@ -0,0 +1,28 @@
+package Synchronisation.ProducerConsumer;
+
+import java.util.Queue;
+
+public class Consumer implements Runnable{
+    private Queue<Object> queue;
+    private int maxSize;
+    private String name;
+
+    public Consumer(Queue queue, int maxSize, String name) {
+        this.queue = queue;
+        this.maxSize = maxSize;
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            synchronized (queue)  {
+                // critical section
+                if (queue.size() > 0) {
+                    System.out.println(this.name + " removing an object. Queue size: " + this.queue.size());
+                    queue.remove();
+                }
+            }
+        }
+    }
+}
---
+++ src/Synchronisation/ProducerConsumer/Producer.java
@@ -0,0 +1,29 @@
+package Synchronisation.ProducerConsumer;
+
+import java.util.Queue;
+
+public class Producer implements Runnable{
+    private Queue<Object> queue;
+    private int maxSize;
+    private String name;
+
+    public Producer(Queue queue, int maxSize, String name) {
+        this.queue = queue;
+        this.maxSize = maxSize;
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            synchronized (queue) {
+                // critical section
+                if (queue.size() < this.maxSize) {
+
+                    System.out.println(this.name + " adding an object. Queue size: " + this.queue.size());
+                    queue.add(new Object());
+                }
+            }
+        }
+    }
+}
